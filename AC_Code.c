dea364071179556a5a7e81ef67cbc45580486f71
Yiting <yiting911022@gmail.com>
2022-09-16 23:36:52 +0800

    fix rebase error

--- AC_Code.c
+++ AC_Code.c
@@ -3,19 +3,11 @@
 int main(){
  
     int n;
-<<<<<<< HEAD
-    scanf("%d%d",&n);
-    int sum=0;
-    int input;
-    for(int i=0; i<n; i++){
-        scanf("%d",&input):
-=======
     scanf("%d",&n);
     int sum=0;
     int input;
     for(int i=0; i<n; i++){
         scanf("%d",&input);
->>>>>>> 0c88bb5 (Yiting 9/16 22:49)
         sum+=input;
     }
     printf("%d",sum);
